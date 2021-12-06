5973de99c261ad9ebd5d7eaf9faa6dbe420796a6
Mpendulo Sibiya <mpendulodev@gmail.com>
2021-12-06 12:02:59 +0200

    nextjs guide from the netninja

---
+++ pages/about.js
@@ -0,0 +1,18 @@
+import Head from "next/head"
+
+function about() {
+    return (
+        <>
+            <Head>
+                <title>About | Ninja</title>
+                <meta name='keywords' content='ninjas'/>
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil accusantium laborum veritatis minima fuga repellendus pariatur, ipsum aliquid? Dignissimos sapiente et voluptatem non aliquid omnis, eos harum eveniet molestias laboriosam!</p>
+            </div>
+        </>
+    )
+}
+
+export default about

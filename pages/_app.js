9441fc22d18a9f5a2629fd93b6ba693dad6fd35c
Mpendulo Sibiya <mpendulodev@gmail.com>
2022-07-31 19:44:11 +0200

    added loader for API and routing between pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,28 @@
 import Layout from '../components/Layout'
 import '../styles/globals.css'
+import 'nprogress/nprogress.css'
+import Router from 'next/router'
+import React from 'react'
+import NProgress from 'nprogress'
 
 function MyApp({ Component, pageProps }) {
-  return( 
+  React.useEffect(() => {
+    const handleRouteStart = () => NProgress.start()
+    const handleRouteDone = () => NProgress.done()
+
+    Router.events.on("routeChangeStart", handleRouteStart)
+    Router.events.on("routeChangeComplete", handleRouteDone)
+    Router.events.on("routeChangeError", handleRouteDone)
+
+    return () => {
+      // Make sure to remove the event handler on unmount!
+      Router.events.off("routeChangeStart", handleRouteStart)
+      Router.events.off("routeChangeComplete", handleRouteDone)
+      Router.events.off("routeChangeError", handleRouteDone)
+    };
+  }, []);
+
+  return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
